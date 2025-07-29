f9619c927e244d18e4386b15eb0b11cc4646287e
flora200198 <aflora398@gmail.com>
2025-07-29 14:25:56 +0530

    checkout completed

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -4,7 +4,6 @@ import ContactForm from '../../components/ContactForm/ContactForm';
 const ContactUs = () => {
   return (
     <div className="container">
-      <h1>Contact Us</h1>
       <ContactForm />
     </div>
   );
