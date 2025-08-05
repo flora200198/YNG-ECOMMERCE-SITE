a5c305eee2ecf38d0e3ad3676f2b65e2df4b950a
flora200198 <aflora398@gmail.com>
2025-08-05 12:43:07 +0530

    thank you page updated

--- src/Pages/Thankyou/Thankyou.jsx
+++ src/Pages/Thankyou/Thankyou.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Thankyou = () => {
   return (
-    <div className="thankyou-page text-center">
+    <div className="thankyou-page text-center mt-5">
       <h1>Thank You!</h1>
       <p>Your order has been Placed.</p>
-      <button className="btn btn-outline-primary" onClick={() => window.location.href = '/'}>Go to Home</button>
+      <button className="btn btn-outline-primary  mb-5" onClick={() => window.location.href = '/'}>Go to Home</button>
     </div>
   );
 }   
