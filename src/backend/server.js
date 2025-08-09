b0765fd41e2d3ae6a3832dadcd3835b3ba87c8cd
flora200198 <aflora398@gmail.com>
2025-08-09 16:55:28 +0530

    server running

---
+++ src/backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+    
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}
+);
+module.exports = app; // Export the app for testing or further configuration
