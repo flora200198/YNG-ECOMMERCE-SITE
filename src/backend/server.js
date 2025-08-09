a9458bb367213b3d31a7163b3a165e948e00cf92
flora200198 <aflora398@gmail.com>
2025-08-09 16:59:00 +0530

    dotenv added

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
     });
 
-    
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 }
