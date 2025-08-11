09e1d2e55cac632a4570d5cbfd169f12d3c8e9aa
flora200198 <aflora398@gmail.com>
2025-08-11 09:51:31 +0530

    Footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,12 +5,13 @@ import './Footer.css';
 const Footer = () => {
   return (
     <>
-  <footer className="custom-footer  text-white pt-5"  style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Logo.png)`,
+  <footer className="custom-footer  pt-5"  style={{
+       
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "contain",
-        minHeight: "200px"
+        minHeight: "200px",
+        color: "black",
       }}>
     <div className="container">
       <div className="row">
@@ -39,19 +40,19 @@ const Footer = () => {
         </div>
 
         {/* Quick Links */}
-        <div className="col-md-4 mb-4">
-          <h5 className="text-white mb-3">Quick Links</h5>
-          <ul className="list-unstyled">
-            <li><Link to="/" className="text-white text-decoration-none">Home</Link></li>
-            <li><Link to="/products" className="text-white text-decoration-none">Products</Link></li>
-            <li><Link to="/about" className="text-white text-decoration-none">About</Link></li>
-            <li><Link to="/blog" className="text-white text-decoration-none">Blog</Link></li>
+        <div className="col-md-4 mb-4" style={{color: "black"}} >
+          <h5 className="mb-3" >Quick Links</h5>
+          <ul className="list-unstyled" >
+            <li><Link to="/" className=" text-decoration-none" style={{color: "black"}}>Home</Link></li>
+            <li><Link to="/products" className=" text-decoration-none" style={{color: "black"}}>Products</Link></li>
+            <li><Link to="/about" className=" text-decoration-none" style={{color: "black"}}>About</Link></li>
+            <li><Link to="/blog" className="text-decoration-none" style={{color: "black"}}>Blog</Link></li>
           </ul>
         </div>
 
         {/* Contact Info */}
         <div className="col-md-4 mb-4">
-          <h5 className="text-white mb-3">Contact Us</h5>
+          <h5 className=" mb-3">Contact Us</h5>
           <p><i className="bi bi-geo-alt-fill me-2"></i>123 Industrial Lane, Tech City, Country</p>
           <p><i className="bi bi-telephone-fill me-2"></i>+91 98765 43210</p>
           <p><i className="bi bi-envelope-fill me-2"></i>info@company.com</p>
